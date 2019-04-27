9572604ecbf8682219d136a400455b57639169ea
Antoine Latrille <antoine.latrille@gmail.com>
2019-04-28 00:22:32 +0200

    Factor out file read

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,30 @@ fn main() {
     day1::run("inputs/day1.txt");
 }
 
-pub mod day1 {
-    use hashbrown::HashSet;
+mod utils {
     use std::fs;
 
-    pub fn run(filename: &str) -> (i32, i32) {
-        let input = fs::read_to_string(filename)
-            .expect(format!("day1: Unable to read file {}", filename).as_str());
+    pub fn read_lines(filename: &str) -> Vec<String> {
+        fs::read_to_string(filename)
+            .expect(format!("day1: Unable to read file {}", filename).as_str())
+            .lines()
+            .map(|line: &str| String::from(line))
+            .collect()
+    }
+}
 
-        let int_vec = input
-            .split("\n")
-            .map(|line: &str| String::from(line).trim().parse::<i32>().unwrap())
+pub mod day1 {
+    use crate::utils::read_lines;
+    use hashbrown::HashSet;
+
+    pub fn run(filename: &str) -> (i32, i32) {
+        let input = read_lines(filename)
+            .iter()
+            .map(|line: &String| line.trim().parse::<i32>().unwrap())
             .collect();
 
-        let sum = day1_sum(&int_vec);
-        let first_cycle = day1_frequency(&int_vec);
+        let sum = day1_sum(&input);
+        let first_cycle = day1_frequency(&input);
 
         println!("Day 1 : Sum is {}, first cycle is {}", sum, first_cycle);
         (sum, first_cycle)
